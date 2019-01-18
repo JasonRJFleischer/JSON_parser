0f67e2828714d42e06c83f7767347cc4e6780191
JasonRJFLeischer <jrjflei@gmail.com>
2019-01-18 10:24:38 -0500

    Skeleton for parsing JSONs into XML readable representation

--- json_parse_combine.py
+++ json_parse_combine.py
@@ -22,7 +22,7 @@ ws: EMPTY | tab ws | lf ws | cr ws | sp ws;
 
 terminals
 true: 'true';
-flase: 'false';
+false: 'false';
 null: 'null';
 tab: /[\u0009]/;
 lf: /[\u000a]/;
@@ -78,7 +78,7 @@ actions = {
 } 
 
 # JSON to test on
-json = '{"colors": [{"color": "black", "category": "hue", "type": "primary", "code": {"rgba": [255, 255, 255, 1], "hex": "#000"}}, {"color": "white", "category": "value", "code": {"rgba": [0, 0, 0, 1], "hex": "#FFF"}}, {"color": "red", "category": "hue", "type": "primary", "code": {"rgba": [255, 0, 0, 1], "hex": "#FF0"}}, {"color": "blue", "category": "hue", "type": "primary", "code": {"rgba": [0, 0, 255, 1], "hex": "#00F"}}, {"color": "yellow", "category": "hue", "type": "primary", "code": {"rgba": [255, 255, 0, 1], "hex": "#FF0"}}, {"color": "green", "category": "hue", "type": "secondary", "code": {"rgba": [0, 255, 0, 1], "hex": "#0F0"}}]}'
+json = '{"colors": [{"color": "black", "category": "hue", "type": "primary", "code": {"rgba": [255, 255, 255, 1], "hex": "#000"}}, {"color": "white", "category": "value", "code": {"rgba": [0, 0, 0, 1], "hex": "#FFF"}}, {"color": "red", "category": "hue", "type": "primary", "code": {"rgba": [255, 0, 0, 1], "hex": "#FF0"}}, {"color": "blue", "category": "hue", "type": "primary", "code": {"rgba": [0, 0, 255, 1], "hex": "#00F"}}, {"color": "yellow", "category": "hue", "type": "primary", "code": {"rgba": [255, 255, 0, 1], "hex": "#FF0"}}, {"color": "green", "category": "hue", "type": true, "code": {"rgba": [0, 255, 0, 1], "hex": "#0F0"}}]}'
 
 g = Grammar.from_string(grammar)
 
